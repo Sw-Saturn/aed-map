3df91d31982f7ab6d6c4f26e539e08549f6646f3
Kanta Demizu <demizu.1203@gmail.com>
2019-06-21 10:17:13 +0900

    Initial commit

---
+++ AED-Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AED-Map
+//
+//  Created by Kanta Demizu on 2019/06/21.
+//  Copyright © 2019 Kanta Demizu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
