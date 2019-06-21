fd8b013cd180f5eda6dd25f297f93de02b8acac9
Kanta Demizu <demizu.1203@gmail.com>
2019-06-21 10:43:44 +0900

    [ADD] MapView

--- AED-Map/ViewController.swift
+++ AED-Map/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var MapView:MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        MapView.userTrackingMode = MKUserTrackingMode.follow
+        MapView.setCenter(MapView.userLocation.coordinate, animated: true)
     }
-
-
 }
 
+
+extension ViewController:CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // 現在位置がlocationsに格納
+        if let coordinate = locations.last?.coordinate{
+            //現在位置の拡大表示
+            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            let region = MKCoordinateRegion(center: coordinate, span: span)
+            MapView.region = region
+        }
+    }
+}
